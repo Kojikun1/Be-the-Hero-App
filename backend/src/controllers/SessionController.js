f95203596aa38edc4c739ace26c5b9fbaadc5e12
Kojikun1 <bruno.chen.duarte@hotmail.com>
2020-05-27 17:53:45 -0300

    Backend Aplication

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,19 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async store(req,res){
+
+        const { id } = req.body;
+
+        const ong = await connection('ongs')
+        .where('id', id)
+        .select('name')
+        .first();
+
+        if(!ong){
+            return res.status(400).json({message: "Error can not found user"});
+        }
+
+        return res.json(ong);
+    }
+}
